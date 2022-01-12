6432071b4ef93f651507f7cb5411ff7872157ded
marccuesta99 <61467000+marccuesta99@users.noreply.github.com>
2022-01-12 20:52:55 +0100

    first commit

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+// Imports dotenv library (this library loads into the app all environment variables located on .env file)
+require("dotenv").config();
+
+// Creates a mongoose instance and connects to MongoDB
+const mongoose = require("mongoose");
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+mongoose.connect(process.env.DB_HOST, options);
+const mongo = mongoose.connection;
+mongo.on('error', (err) => console.error(err));
+mongo.once('open', () => {
+  console.log('Connected to TalludoDB')
+})
+
+// Creates an express instance and sets it up
+const express = require("express");
+const app = express();
+
+const { json, urlencoded } = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.disable("x-powered-by Nuclio");
+
+// Add services routes to express instance
+const services = require("./src/routers/services.router");
+app.use("/services", services);
+
+// Function that will start the express server when called
+app.listen(5001, () => {
+  console.log(`REST API listening on http://localhost:5001/`);
+});
