cf7e6499974eda2d3b3a4accac8ffc69e9ee23f7
jlcrayo <jlcrayo@gmail.com>
2022-03-14 15:45:59 +0100

    actualizando admin

--- src/routers/airlines.router.js
+++ src/routers/airlines.router.js
@@ -2,16 +2,17 @@ const express = require("express");
 const router = express.Router();
 const airlineController = require("../controllers/airline.controller");
 const {authMiddleware} = require('../middlewares/authMiddleware');
+const {isAdmin} = require('../middlewares/authMiddleware');
 router
   .route("/")
   .get(authMiddleware, airlineController.all)
-  .post(authMiddleware, airlineController.create);
+  .post(authMiddleware,isAdmin, airlineController.create);
 
 router
   .route("/:id")
   .get(airlineController.get)
-  .delete(authMiddleware, airlineController.remove)
-  .put(authMiddleware, airlineController.update);
+  .delete(authMiddleware,isAdmin, airlineController.remove)
+  .put(authMiddleware,isAdmin, airlineController.update);
 
 
 router.route("/search/:text").get(airlineController.search);
