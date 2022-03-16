397f06fd0a418a04b6c7c0da58f375efa6d90ff4
jlcrayo <jlcrayo@gmail.com>
2022-03-16 14:17:12 +0100

    quitando comentarios

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -3,13 +3,9 @@ const User = require('../models/user.model');
 import Role from "../models/role.model";
 
 exports.authMiddleware = async (req, res, next)=> {
-    // Token is inside the "Authorization" header, with a value like this:
-    // "Bearer ey123bcashkjhacakb.asdasdasdas.asdasdasd"
-    // We split that string by a space (" ") and retrieve the second item (the token itself)
+    
     const token = req.headers.authorization?.split(" ")[1];
 
-    // The verify() method will throw an error if the token is invalid, so we use
-    // a try/catch block to handle the error thrown and return 400
     let tokenData;
     try {
         tokenData = jwt.verify(token, process.env.JWT_SECRET);
